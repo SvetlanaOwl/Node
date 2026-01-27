b9b5608ac31daa7068c6aa3edbab4a315d64d533
Светлана <fox.2025.owl@gmail.com>
2026-01-27 18:52:30 +1000

    модули

---
+++ simpleWebServer.js
@@ -0,0 +1,6 @@
+const { write } = require('fs');
+const http = require('http');
+http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello from my web server!');
+}).listen(8080);
---
+++ utils.js
@@ -0,0 +1,19 @@
+// Exporting multiple function
+// Экспорт нескольких функций
+const getCurrentDate = () => new Date().toISOString();
+
+const formatCurrency = (amount, currency = 'USD') => {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: currency
+  }).format(amount);
+};
+
+// Method 1: Exporting multiple item
+// Метод 1: Экспорт нескольких элементов
+exports.getCurrentDate = getCurrentDate;
+exports.formatCurrency = formatCurrency;
+
+// Method 2: Exporting an object with multiple properties
+// Метод 2: Экспорт объекта с несколькими свойствами
+// module.exports = { getCurrentDate, formatCurrency };
