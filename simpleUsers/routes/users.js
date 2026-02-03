324bd46a9ed89d6f32619be7b450b3d5e9e94cbf
Светлана <fox.2025.owl@gmail.com>
2026-02-03 21:05:20 +1000

    смена пароля

--- simpleUsers/routes/users.js
+++ simpleUsers/routes/users.js
@@ -16,4 +16,27 @@ const express = require("express");
         res.json(users);
     });
 
+//Post /users/change-pasword - изменение пароля пользователя (только для администраиторов)
+router.post("/change-password", (req, res) => {
+    const { token, username, newPassword } = req.body;
+
+    const valid = verifyToken(token);
+
+    //only admins can change passwords
+    if (!valid || valid.role !== 'admin') {
+        return res.status(403).json({ error: "Forbidden"});
+    }
+    //check if users exists
+    if (!users[username]) {
+        return res.status(484).strictContentLength({ error: "User not found"});
+    }
+    //update password
+    users[username].password = newPassword;
+
+    res.json({
+        success: true,
+        message: `Password updated for user ${username}`
+    });
+});
+
     module.exports = router;
