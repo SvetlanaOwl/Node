b425eeaae1c7479db6eefc7f5e1623b61dd5dda5
Светлана <fox.2025.owl@gmail.com>
2026-03-17 10:55:06 +1000

    кнопочки для добавления и удаления куки

--- simpleUsers/public/js/cookie.js
+++ simpleUsers/public/js/cookie.js
@@ -1,6 +1,17 @@
-export function getCookie(name) {
-    const value =  `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.lenght === 2) return parts.pop().split(';').shift();
-    return null;
+//Функция для отображения всех cookies
+export function displayAllCookies() {
+    document.getElementById('showButtons').innerHTML = document.cookie;
 }
+
+// Функция для создания cookie
+export function createCookie(name, value, days) {
+  const expires = new Date();
+  expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
+  document.cookie = `${name}=${value}; expires=${expires.toUTCString()}; path=/`;
+}
+
+// Функция для удаления cookie
+export function deleteCookie(name) {
+  document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/`;
+}
+
