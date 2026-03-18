6e97b653dfaaa6653a8ce39275e1f95ebea672e9
Светлана <fox.2025.owl@gmail.com>
2026-03-18 11:16:59 +1000

    создание фунции в которой константы и слухачи

--- simpleUsers/public/js/buttonManager.js
+++ simpleUsers/public/js/buttonManager.js
@@ -1,10 +1,10 @@
  import { displayAllCookies, createCookie, deleteCookie } from "./cookie.js";
 
- // Создание переменных для кнопок 
-const btnCreate1 = document.getElementById('createCookie1');
-const btnCreate2 = document.getElementById('createCookie2');
-const btnDelete1 = document.getElementById('deleteCookie1');
-const btnDelete2 = document.getElementById('deleteCookie2');
+export function buttonListeners() {
+    const btnCreate1 = document.getElementById('createCookie1');
+    const btnCreate2 = document.getElementById('createCookie2');
+    const btnDelete1 = document.getElementById('deleteCookie1');
+    const btnDelete2 = document.getElementById('deleteCookie2');
 
 
  // Обработчики кликов для кнопок
@@ -28,9 +28,4 @@ deleteCookie('cookie2');
 displayAllCookies();
 });
 
-export {
-  btnCreate1,
-  btnCreate2,
-  btnDelete1,
-  btnDelete2
-};
+}
